e7b5201557a67689093f8aedc650c47999e8e6c0
Leoendithas <122543071+Leoendithas@users.noreply.github.com>
2025-03-19 16:01:09 +0800

    v2.1

--- app copy.py
+++ app copy.py
@@ -274,10 +274,10 @@ if st.session_state.story_data:
                 .enter()
                 .append("path")
                 .attr("class", "link")
-                .attr("d", function(d) {
+                .attr("d", function(d) {{
                     return "M" + d.source.x + "," + d.source.y +
                            "L" + d.target.x + "," + d.target.y;
-                });
+                }});
             
             // Create node groups
             const node = svg.selectAll(".node")
